e43a712431c0ac2a4e285b736df2315d018305dd
sarah <alexandraamani@icloud.com>
2025-02-25 01:29:18 -0500

    Login and create account

---
+++ app/src/main/java/com/example/coen390_feverapp/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.coen390_feverapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnlogin;
+    DBHelper dbHelper;
+    EditText etUsername, etPwd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        dbHelper= new DBHelper(this);
+        etUsername=findViewById(R.id.etUsername);
+        etPwd=findViewById(R.id.etPassword);
+        btnlogin=findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isLoggedId = dbHelper.checkUser(etUsername.getText().toString(),etPwd.getText().toString());
+                if(isLoggedId){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "login failed ",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/coen390_feverapp/RegisterActivity.java
@@ -0,0 +1,75 @@
+package com.example.coen390_feverapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText etUser,etPwd,etRepwd;
+    Button btnRegister, bntGoToLogin;
+    DBHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        etUser=findViewById(R.id.editTextText);
+        etPwd=findViewById(R.id.editTextText2);
+        etRepwd=findViewById(R.id.editTextText3);
+        btnRegister=findViewById(R.id.button1);
+        dbHelper= new DBHelper(this);
+        bntGoToLogin=findViewById(R.id.button2);
+        bntGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String user, pwd, rePwd;
+                user = etUser.getText().toString();
+                pwd = etPwd.getText().toString();
+                rePwd = etRepwd.getText().toString();
+                if (user.equals("")||pwd.equals("")||rePwd.equals("")){
+                    Toast.makeText(RegisterActivity.this, "please fill all the fields",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    if(pwd.equals(rePwd)){
+                        if(dbHelper.checkUsername(user)){
+                            Toast.makeText(RegisterActivity.this, "User Already Exist",Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                       boolean registeredSuccess=dbHelper.insertData(user,pwd);
+                       if(registeredSuccess){
+                           Toast.makeText(RegisterActivity.this, "User Registered Successfully ",Toast.LENGTH_LONG).show();
+                       }
+                       else {
+                           Toast.makeText(RegisterActivity.this, "User Registered Failed ",Toast.LENGTH_LONG).show();
+                       }
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this, "try again passwords doesn't match ",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+            }
+        });
+
+
+
+
+    }
+}
