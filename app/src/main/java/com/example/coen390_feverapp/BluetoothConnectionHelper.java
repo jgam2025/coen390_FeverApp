3c9e0e55eb4d3fb84bd240309105bba1ebb23691
jgam2025 <jeffgam6@gmail.com>
2025-02-27 16:58:30 -0500

    Activity_Scan_Measurement UI & Bluetooth Integration

---
+++ app/src/main/java/com/example/coen390_feverapp/BluetoothConnectionHelper.java
@@ -0,0 +1,5 @@
+package com.example.coen390_feverapp;
+
+public class BluetoothConnectionHelper {
+    //TODO: Bring all bluetooth methods here
+}
--- app/src/main/java/com/example/coen390_feverapp/ScanMeasurementActivity.java
+++ app/src/main/java/com/example/coen390_feverapp/ScanMeasurementActivity.java
@@ -9,10 +9,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,16 +27,26 @@ import java.io.OutputStream;
 import java.util.UUID;
 
 public class ScanMeasurementActivity extends AppCompatActivity {
-    private TextView textViewShowTemp;
-    private Button buttonSaveTemp;
 
+
+    // Bluetooth Variables
     private static final String DEVICE_NAME = "ESP32";
     private static final UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothSocket socket;
     private InputStream inputStream;
+
+    // UI Variables
+    protected TextView textViewTemperature;
+    protected ProgressBar measurementProgressBar;
+    protected LinearLayout cancelAndSaveButtonLayout;
+    protected Button buttonStart, buttonCancel, buttonSave;
+
+    // Other Variables
+    protected boolean measurementCanceled = false;
+    protected String temperature;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,25 +62,85 @@ public class ScanMeasurementActivity extends AppCompatActivity {
     }
 
     private void setupUI(){
-        textViewShowTemp = findViewById(R.id.textViewShowTemp);
-        buttonSaveTemp = findViewById(R.id.buttonSaveTemp);
+        textViewTemperature = findViewById(R.id.textViewTemperature);
+        buttonStart = findViewById(R.id.buttonStart);
+        measurementProgressBar = findViewById(R.id.measurementProgressBar);
+        cancelAndSaveButtonLayout = findViewById(R.id.linearLayoutCancelAndSave);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonCancel = findViewById(R.id.buttonCancel);
+        buttonSave = findViewById(R.id.buttonProgressAndSave);
+
+        cancelAndSaveButtonLayout.setVisibility(View.GONE);
+        buttonStart.setVisibility(View.VISIBLE);
+
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null){
-            Toast.makeText(this, "Bluetooth not supposed", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
             finish();
         }
 
-        buttonSaveTemp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("DEBUG", "Button clicked!");  // Debugging log
-                Toast.makeText(ScanMeasurementActivity.this, "Connecting...", Toast.LENGTH_SHORT).show();
-                connectToESP32();
-            }
-        });
+        buttonStart.setOnClickListener(v -> startMeasurement());
+        buttonCancel.setOnClickListener(v -> cancelMeasurement());
+        buttonSave.setOnClickListener(v -> saveMeasurement());
 
     }
+    private void startMeasurement(){
+        buttonStart.setVisibility(View.GONE);
+        cancelAndSaveButtonLayout.setVisibility(View.VISIBLE);
+        buttonSave.setClickable(false);
+        measurementProgressBar.setProgress(0);
+
+        Log.d("DEBUG", "Button clicked!");
+        Toast.makeText(ScanMeasurementActivity.this, "Connecting...", Toast.LENGTH_SHORT).show();
+        connectToESP32();
+
+        //start measuring for 8 seconds
+        long a, b;
+        int duration = 8000; // time in milliseconds Ex:8000ms=8s
+        a = System.currentTimeMillis();
+        while(true) {
+            if (measurementCanceled) break;
+
+            // for 8 seconds
+            b = System.currentTimeMillis();
+            if (b - a >= duration) break;
+
+            // Bluetooth measure
+            temperature = readTemperature();
+            if (temperature == null) {
+                Toast.makeText(this, "Error reading data", Toast.LENGTH_SHORT).show();
+                temperature = "Null";
+            }
+
+            // display measure
+            textViewTemperature.setText(temperature + " °C");
+
+            // display progression
+            measurementProgressBar.setProgress((int)((b - a)/duration)*100,true);
+            buttonSave.setText(((int)((b - a)/duration)*100)+"%");
+        }
+
+        //* activate save button and display "save" instead of percentage
+        buttonSave.setText("save");
+        buttonSave.setClickable(true);
+    }
+
+    private void cancelMeasurement(){
+        cancelAndSaveButtonLayout.setVisibility(View.GONE);
+        buttonStart.setVisibility(View.VISIBLE);
+        measurementCanceled = true;
+        textViewTemperature.setText("0 °C");
+    }
+    private void saveMeasurement(){
+        //TODO: create a way to save the data on a database
+    }
+
+    /*
+    *
+    * All Bluetooth methods are below this comment
+    *
+    */
 
     private void connectToESP32() {
         BluetoothDevice device = null;
@@ -96,30 +166,27 @@ public class ScanMeasurementActivity extends AppCompatActivity {
             socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
             socket.connect();
             inputStream = socket.getInputStream();
-
-            // Send the request for temperature
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write("GET_TEMP\n".getBytes()); // Send command to ESP32
-            outputStream.flush();
-
-            readTemperature(); // Read the response from ESP32
         } catch (IOException e) {
             Toast.makeText(this, "Failed to connect", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private String readTemperature() {
+        try {
+            // Send the request for temperature
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("GET_TEMP\n".getBytes()); // Send command to ESP32
+            outputStream.flush();
 
-    private void readTemperature() {
-        new Thread(() -> {
-            try {
-                byte[] buffer = new byte[256];
-                int bytesRead = inputStream.read(buffer);
-                String receivedData = new String(buffer, 0, bytesRead).trim();
+            // Read incoming temperature
+            byte[] buffer = new byte[256];
+            int bytesRead = inputStream.read(buffer);
+            String receivedData = new String(buffer, 0, bytesRead).trim();
 
-                runOnUiThread(() -> textViewShowTemp.setText(receivedData + " °C")); // Update UI
-            } catch (IOException e) {
-                runOnUiThread(() -> Toast.makeText(this, "Error reading data", Toast.LENGTH_SHORT).show());
-            }
-        }).start();
+            return receivedData;
+
+        }catch (IOException e) {
+            return null;
+        }
     }
 }
