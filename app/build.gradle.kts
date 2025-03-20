16207a8d4eef24acfcebf7259dcac32d4299e58e
A-Toslim <amiratoslim2002@gmail.com>
2025-03-20 17:23:33 -0400

    library import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Graph Library:
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 }
