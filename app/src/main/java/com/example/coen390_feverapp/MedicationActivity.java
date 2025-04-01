ea019c16491419536069237aab0924de8bd2661c
Charlotte <charlotteemilyglen@gmail.com>
2025-03-31 20:50:04 -0400

    user has option to choose med from spinner or edittext; option to save edittext field into db

--- app/src/main/java/com/example/coen390_feverapp/MedicationActivity.java
+++ app/src/main/java/com/example/coen390_feverapp/MedicationActivity.java
@@ -29,6 +29,7 @@ public class MedicationActivity extends AppCompatActivity {
     private Button submitMedButton;
     private Spinner medsSpinner, profilesSpinner;
     private DBHelper dbHelper;
+    private String medicationNameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,24 +50,38 @@ public class MedicationActivity extends AppCompatActivity {
         submitMedButton = findViewById(R.id.submitMedButton);
         submitMedButton.setOnClickListener(v -> {
 
-            SharedPreferences sharedPrefsForMeds = getSharedPreferences("medication_prefs", MODE_PRIVATE);
-            String medicationNameSpinner = sharedPrefsForMeds.getString("chosen_medication", null);
-            String medicationNameText = medsEditText.getText().toString().trim();
+            //SharedPreferences sharedPrefsForMeds = getSharedPreferences("medication_prefs", MODE_PRIVATE);
+            String medicationNameSpinner = (String) medsSpinner.getSelectedItem();
+            medicationNameText = medsEditText.getText().toString().trim();
             String medicationDose = doseEditText.getText().toString().trim();
 
-            if ((!medicationNameText.isEmpty()) ^ (!medicationNameSpinner.isEmpty())) { // XOR - only one can be chosen
+            if ((!medicationNameText.isEmpty() && medicationNameSpinner == null) ||
+                    (medicationNameSpinner != null && !medicationNameSpinner.isEmpty() && medicationNameText.isEmpty())) {
 
                 SharedPreferences sharedPrefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
                 String currentProfile = sharedPrefs.getString("current_profile", "default");
 
-                boolean inserted = dbHelper.insertMedication(currentProfile, medicationNameText, medicationDose);
-                if(inserted){
-                    Toast.makeText(this, "Medication saved!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Error saving medication", Toast.LENGTH_SHORT).show();
+                if(!medicationNameText.isEmpty()) { // selected medication from edit text
+                    SaveNewMedsFragment newMedsFragment = new SaveNewMedsFragment();
+                    newMedsFragment.show(getFragmentManager(), "NewMedication");
+                    boolean inserted = dbHelper.insertMedication(currentProfile, medicationNameText, medicationDose);
+                    if (inserted) {
+                        Toast.makeText(this, "Medication saved!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Error saving medication", Toast.LENGTH_SHORT).show();
+                    }
+                    medsEditText.setText("");
+                    doseEditText.setText("");
+                } else if (medicationNameSpinner!=null && !medicationNameSpinner.isEmpty()){ // selected medication from spinner
+                    boolean inserted = dbHelper.insertMedication(currentProfile, medicationNameSpinner, medicationDose);
+                    if (inserted) {
+                        Toast.makeText(this, "Medication saved!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Error saving medication", Toast.LENGTH_SHORT).show();
+                    }
+                    medsEditText.setText("");
+                    doseEditText.setText("");
                 }
-                medsEditText.setText("");
-                doseEditText.setText("");
             } else {
                 Toast.makeText(this, "Please enter a medication name", Toast.LENGTH_SHORT).show();
             }
@@ -74,12 +89,13 @@ public class MedicationActivity extends AppCompatActivity {
 
     }
 
+
     private void showProfilesOnSpinner(){
         SharedPreferences sharedPrefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
         String currentUser = sharedPrefs.getString("current_user",null);
         List<String> profileList = dbHelper.getProfiles(currentUser);
         if(profileList.isEmpty()){
-            //do nothing
+            //todo: message saying its empty
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,profileList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -101,7 +117,6 @@ public class MedicationActivity extends AppCompatActivity {
 
     private void showMedicationsOnSpinner(){
         SharedPreferences sharedPrefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
-        SharedPreferences sharedPrefsForMeds = getSharedPreferences("medication_prefs", MODE_PRIVATE);
         String currentUser = sharedPrefs.getString("current_user",null);
         int userID = dbHelper.getUserID(currentUser);
         List<String> medicationList = dbHelper.getUserAddedMedications(userID);
@@ -115,14 +130,15 @@ public class MedicationActivity extends AppCompatActivity {
         medsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedMedication = (String) parent.getItemAtPosition(position);
-                SharedPreferences.Editor editor = sharedPrefsForMeds.edit();
-                editor.putString("selected_medication", selectedMedication);
-                editor.apply();
+
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) { }
         });
+    }
+
+    public String getMedicationNameText() {
+        return medicationNameText;
     }
 
     /*
@@ -156,10 +172,8 @@ public class MedicationActivity extends AppCompatActivity {
         });
 
     }
-
-
-
      */
+
 
 
     //toolbar menu items
---
+++ app/src/main/java/com/example/coen390_feverapp/SaveNewMedsFragment.java
@@ -0,0 +1,72 @@
+package com.example.coen390_feverapp;
+
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class SaveNewMedsFragment extends DialogFragment {
+
+    private Button yesButton, noButton;
+    private MedicationActivity medicationActivity;
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context instanceof MedicationActivity){
+            medicationActivity = (MedicationActivity) context;
+        }
+    }
+
+    public SaveNewMedsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_save_new_meds, container, false);
+
+        yesButton = view.findViewById(R.id.yesButton);
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPrefs = getActivity().getSharedPreferences("user_prefs", 0);
+                String currentUser = sharedPrefs.getString("current_user",null);
+                Log.d("current_user_check",currentUser);
+                DBHelper dbHelper = new DBHelper(getActivity().getBaseContext());
+                int userID = dbHelper.getUserID(currentUser);
+                if(medicationActivity != null){
+                    boolean inserted = dbHelper.insertNewMedication(medicationActivity.getMedicationNameText(), userID);
+                    if(inserted){
+                        Toast.makeText(getContext(), "New medication saved!", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(getContext(),"Error inserting medication", Toast.LENGTH_LONG).show();
+                    }
+                }
+                //save into db
+                dismiss();
+            }
+        });
+
+        noButton = view.findViewById(R.id.noButton);
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+}
