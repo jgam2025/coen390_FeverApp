cc7d97c4e51e8902ab8f47190b4938b8791f604f
A-Toslim <amiratoslim2002@gmail.com>
2025-04-07 02:29:13 -0400

    check

--- app/src/main/java/com/example/coen390_feverapp/BaseActivity.java
+++ app/src/main/java/com/example/coen390_feverapp/BaseActivity.java
@@ -3,6 +3,8 @@ package com.example.coen390_feverapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -28,6 +31,11 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_base);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false); // Hide the title
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,15 +43,51 @@ public class BaseActivity extends AppCompatActivity {
         });
         setupUI();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu from the menu.xml file in the menu directory
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.miperson) {
+            goToHealth();
+            return true;
+
+        }
+
+        else if (id == R.id.miMore) {
+            GoToExtra();
+            return true;
+
+
+        } else{
+            return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void GoToExtra(){
+        Intent intent = new Intent(this, ExtraPageActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    private void goToHealth(){
+        Intent intent = new Intent(this, HealthDataActivity.class);
+        startActivity(intent);
+    }
 
     void setupUI(){
         SharedPreferences sharedPrefs = getSharedPreferences("user_prefs", 0);
         String currentUser = sharedPrefs.getString("current_user",null);
         homeTextView = findViewById(R.id.homeTextView);
         homeTextView.setText("Welcome, " + currentUser + "!");
-
-        String date = new SimpleDateFormat("M dd, yyyy", Locale.getDefault()).format(new Date());
-        //todo: put month into words
 
 
 
@@ -86,9 +130,7 @@ public class BaseActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    void goToHistory(){
-        //TODO: create history page
-    }
+
 
     void goToSymptomLog(){
         Intent intent = new Intent(this, SymptomLogActivity.class);
