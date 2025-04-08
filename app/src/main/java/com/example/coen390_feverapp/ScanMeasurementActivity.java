4c5ee4b847a4365bfacfbfb5690d9671bb759d03
alemucc <alexiamucciacciaro@hotmail.com>
2025-04-07 22:11:59 -0400

    scan

--- app/src/main/java/com/example/coen390_feverapp/ScanMeasurementActivity.java
+++ app/src/main/java/com/example/coen390_feverapp/ScanMeasurementActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
@@ -68,11 +71,13 @@ public class ScanMeasurementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_scan_measurement);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.checkBoxLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
 
         setupUI();
 
@@ -93,6 +98,35 @@ public class ScanMeasurementActivity extends AppCompatActivity {
         instructionDialogLayout.setVisibility(android.view.View.VISIBLE);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu from the menu.xml file in the menu directory
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.miperson) {
+            goToHealth();
+            return true;
+
+        } else if (id == R.id.miMore) {
+            GoToExtra();
+            return true;
+
+        } else if (id == R.id.mihome) {
+            goToHome();
+            return true;
+
+
+        }else {
+            return super.onOptionsItemSelected(item);
+
+        }
+    }
     private void setupUI() {
         temperatureTextView = findViewById(R.id.textViewTemperature);
         temperatureTextView2 = findViewById(R.id.textViewTemperature2);
@@ -319,5 +353,23 @@ public class ScanMeasurementActivity extends AppCompatActivity {
         socket = null;
         inputStream = null;
     }
+
+    private void GoToExtra(){
+        Intent intent = new Intent(this, ExtraPageActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    private void goToHealth(){
+        Intent intent = new Intent(this, HealthDataActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToHome(){
+        Intent intent = new Intent(this,BaseActivity.class);
+        startActivity(intent);
+    }
+
 
 }
